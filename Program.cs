004ffb96c8379d767679a8cffef4aeb862dde7ec
zenin5479 <zenin5479@gmail.com>
2025-10-01 09:10:23 +0300

    Update 0.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace MultipleIfStatement
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
         }
