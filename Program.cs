8b1397248f80c02c9c9f554c213abd653aed216c
zenin5479 <zenin5479@gmail.com>
2025-10-01 09:11:58 +0300

    Update 0.2 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -2,11 +2,24 @@
 
 namespace MultipleIfStatement
 {
-    internal class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   internal class Program
+   {
+      static void Main()
+      {
+         int number = 10;
+
+         if (number > 0)
+         {
+            Console.WriteLine("Число положительное");
+         }
+         else if (number < 0)
+         {
+            Console.WriteLine("Число отрицательное");
+         }
+         else
+         {
+            Console.WriteLine("Число равно нулю");
+         }
+      }
+   }
 }
